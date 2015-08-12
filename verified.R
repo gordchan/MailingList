dbbc7eaca209c32bda1044af73617a0e93df6f5f
gordchan <gordon.chan@mensa.org.hk>
2015-08-12 02:16:05 +0000

    refined functions variables

--- verified.R
+++ verified.R
@@ -1,13 +1,11 @@
-verify <- function(x){
-
-  # x = "mailing list.txt"
+verify <- function(file = "mailing list.txt", name = "mailing_list"){
     
   ## Read through email validated by Outlook "To:" field and return whether the email has been validated
   
-  library(dplyr)
-  library(xlsx)
+  require(dplyr)
+  require(xlsx)
   
-  input <- paste("input/", x, sep = "")
+  input <- file.path("input", file)
   
   verified <- scan(input, what = "character", sep=";")
 
@@ -17,8 +15,26 @@ verify <- function(x){
   
   ver_df <- ver_df %>% mutate(valid = grepl("*<*>$", verified))
   
-  write.xlsx(ver_df, file ="output/verified_address.xlsx", sheetName="verified_email", row.names = FALSE)
+  output <- file.path("output", "mailing list.xlsx")
   
+  write.xlsx(ver_df, file = output, sheetName = name, row.names = FALSE)
+  
+    wb <- loadWorkbook(output)
+      sheets <- getSheets(wb)
+        sheet <- sheets[[1]]
+  
+        setColumnWidth(sheet, colIndex=1, colWidth=60)
+        setColumnWidth(sheet, colIndex=2, colWidth=24)
+        
+        row <- getRows(sheet, rowIndex=1)
+        cb <- CellBlock(sheet, startRow=1, startColumn=1, noRows=1, noColumns=2, create=FALSE)
+        
+        header <- Font(wb, isBold=TRUE)
+        
+        CB.setFont(cellBlock=cb, font=header, rowIndex=1, colIndex=1:2)
+        
+    saveWorkbook(wb, output)
+        
 }
 
-verify("mailing list.txt")
+verify(name = "KWC_Q&S_Forum_2016_OC")
